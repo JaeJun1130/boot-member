71b1f474d7a469bb848b4de6d0aee67195451547
jaejun <wjswowns1234@gmail.com>
2021-03-14 21:30:20 +0900

    Repository Map List 구현

---
+++ demo/src/main/java/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.spring.prj.demo.domain.Member;
+
+public interface MemberRepository {
+	Member save(Member member);
+	Optional<Member> findById(Long id);
+	Optional<Member> findName(String name);
+	List<Member> findAllI();
+}
